1327875f699ca126144f0998a8dd73821fc83762
Kirsten Nordine <kirstennordine@gmail.com>
2020-03-28 16:37:41 -0400

    Implement solution for pangram exercise

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        string inputLetters = Regex.Replace(input.ToLower(), @"[^a-z]+", String.Empty);
+
+        return inputLetters.Distinct().Count() == alphabet.Length;
     }
 }
