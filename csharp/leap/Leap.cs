09011ea7345109957dc84d84ff5750c7c099ddbf
Kirsten Nordine <kirstennordine@gmail.com>
2020-04-26 19:05:21 -0400

    [Leap] Extract DivisibleBy method

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,6 +2,11 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        return DivisibleBy(year, 4) &&
+            (!DivisibleBy(year, 100) || DivisibleBy(year, 400));
+    }
+
+    private static bool DivisibleBy(int dividend, int divisor) {
+        return dividend % divisor == 0;
     }
 }
